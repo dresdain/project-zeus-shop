d0f82a68c1d77164144140584b5a4d9daa3cc938
Dresdain <dresdain@live.com>
2019-12-08 01:41:21 +0800

    ✈️ Added plan results list

--- iwov-resources/flp/scripts/marketplace/emp-scripts.js
+++ iwov-resources/flp/scripts/marketplace/emp-scripts.js
@@ -1,3 +1,4 @@
+var showPlanDOM__state = false;
 /* 
 *  slick.js
 *  Init mobile slick
@@ -14,22 +15,68 @@ $('.partners__box--slick').slick({
 *   STUB handle #formPlan submissions
 *
 */
-$('#planForm').on('submit', function(e){
-    console.log(e);
+$('#planForm').on('submit', function(e){ 
     e.preventDefault(); 
     //
-    hideLandingDivs();
-    
-    //
-    showPlanDOM();
+    showLandingDivs(false);
+    showPlanDOM(true);
+    showPlanDOM__state = true;
+});
+
+$('#hidePlans').on('click', function(e){
+    showLandingDivs(true);
+    showPlanDOM(false);
 });
 
 
 /* STUB show/hide functions */
-function hideLandingDivs(){
-    $('.emp__landing').fadeOut();
-    $('.emp__landing ~ .group-tiles').fadeOut();
+function showLandingDivs(state){
+    if(state === true){
+        $('.emp__landing').removeClass('kagebunshin');
+        $('.emp__landing ~ .group-tiles').show();
+    }else{
+        $('.emp__landing').addClass('kagebunshin');;
+        $('.emp__landing ~ .group-tiles').hide();
+    }
 }
-function showPlanDOM(){
-    
+function showPlanDOM(state){
+    if(state === true){
+        $('.emp__results').fadeIn('1000');
+        setTimeout(function(){
+            $('.emp__landing ~ .group-tiles').addClass('kagebunshin');;
+        }, 200);
+    }else{
+        $('.emp__results').fadeOut('1000');
+    }
 }
+
+
+/* STUB onLoad */
+$(window).on('load', function(){
+    $.support.transition = false;
+    //Ensures all script must be loaded before submitting
+    $('#planForm button[type="submit"]').removeAttr('disabled');
+});
+
+/* STUB onReady */
+$(function(){
+    $('[data-toggle="tooltip"]').tooltip(); 
+});
+
+/* STUB onScroll showHide top filter */
+$(window).scroll(function(){  
+    if($(window).width() > 768){
+        if($(window).scrollTop() > 434){
+            if($('.header-placeholder > header.navbar').hasClass('mini-menu')){
+                $('.emp__menu__top').addClass('sticky--nav').removeClass('sticky--nav--shift');
+            }else{
+                $('.emp__menu__top').addClass('sticky--nav--shift');
+            }
+        }else if($(window).scrollTop() < 370){
+            $('.emp__menu__top').removeClass('sticky--nav sticky--nav--shift');
+        }
+    }else{
+        $('.emp__menu__top').removeClass('sticky--nav sticky--nav--shift');
+    }
+});
+ 
--- iwov-resources/flp/scripts/marketplace/emp-tests-sim.js
+++ iwov-resources/flp/scripts/marketplace/emp-tests-sim.js
@@ -1,13 +1,32 @@
+/* 
+* Only for triggerring events and interactions
+* No logic an
+*
+*/
+
 /* STUB on load show/hide */
 function test__showHideDOM(){
-    $('#planForm').trigger('submit')
+    $('#planForm').trigger('submit'); 
 }
 
+function test__duplicateDOM(){
+    for (var index = 0; index < 10; index++) {
+        $("div.emp__results__box--list").append($("#plan_id-1").clone());
+    }
+    $('[data-toggle="tooltip"]').tooltip(); 
+}
 
 
 /* On page load */
 $(window).on('load', function(){
-    test__showHideDOM();
+
+
+    // test__showHideDOM();
+
+
+    test__duplicateDOM();
+
+
 });
 
 /* On DOM load */
